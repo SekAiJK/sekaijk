b419e93849b10bc5fe4f435c37b12a92a483071b
David <46817273+TDPerz@users.noreply.github.com>
2022-09-17 16:47:44 -0600

    Correcion de cloudinary

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 export default {
     GOOGLE_CREDENTIAL:'786126594115-o4opng7ttvj2gb34rc6eg4f4na12ah19.apps.googleusercontent.com',
     API:'https://sekaijk-api-prod.herokuapp.com',
-    API_CLOUDIMARY:'http://api.cloudinary.com/v1_1/sekai-jk',
+    API_CLOUDIMARY:'https://api.cloudinary.com/v1_1/sekai-jk',
     CLOUDINARY_APIKEY:'899818792881952',
     CLOUDINARY_APISECRET: 'meVfGr5QY8piitdbBQhKyudHWMY',
     CLOUD_NAME:'sekai-jk',
